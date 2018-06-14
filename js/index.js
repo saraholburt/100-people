afc36a9d54359fd71305e7fb9530b24cef37f3b8
Sara Holburt <sara.holburt@gmail.com>
2018-06-14 11:32:03 -1000

    Added function for toggling nav

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 });
 
-//About Modal
+//ABOUT MODAL
 var modal = document.getElementById('about-modal');
 document.getElementById('btn-about').onclick = function() {
   modal.style.display = 'block';
@@ -9,4 +9,27 @@ document.getElementById('btn-about').onclick = function() {
 
 document.getElementById('back-btn').onclick = function() {
   modal.style.display = 'none';
+}
+
+//NAVIGATION
+
+var navGender = document.getElementById('nav-gender');
+var navLanguage = document.getElementById('nav-language');
+var navGeo = document.getElementById('nav-geo');
+var navAge = document.getElementById('nav-age');
+var navReligion = document.getElementById('nav-religion');
+var navLiteracy = document.getElementById('nav-literacy');
+var navEdu = document.getElementById('nav-edu');
+var navPoverty = document.getElementById('nav-poverty');
+var navNutrition = document.getElementById('nav-nutrition');
+
+navLanguage.onclick = function() {
+  changeNav(navGeo, navLanguage);
+}
+
+function changeNav(currentNavItem, newNavItem) {
+  if (currentNavItem.classList.contains('nav-current')) {
+    currentNavItem.classList.remove('nav-current');
+    newNavItem.classList.add('nav-current');
+  }
 }
