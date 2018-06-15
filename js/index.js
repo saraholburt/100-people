c946170cab8c40ad80ddbd6ff9f4c067f208077e
Sara Holburt <sara.holburt@gmail.com>
2018-06-15 13:39:16 -1000

    wrapped up nav fix

--- js/index.js
+++ js/index.js
@@ -25,12 +25,65 @@ var navNutrition = document.getElementById('nav-nutrition');
 
 var navList = [navGender, navLanguage, navGeo, navAge, navReligion, navLiteracy, navEdu, navPoverty, navNutrition];
 
-//var navMenu = document.getElementById('nav-menu');
+navList[0].onclick = function() {
+  for (var i = 0; i<navList.length; i++) {
+      removeNavItem(navList[i]);
+      navList[0].classList.add('nav-current');
+  }
+}
 
+navList[1].onclick = function() {
+  for (var i = 0; i<navList.length; i++) {
+      removeNavItem(navList[i]);
+      navList[1].classList.add('nav-current');
+  }
+}
+
+navList[2].onclick = function() {
+  for (var i = 0; i<navList.length; i++) {
+      removeNavItem(navList[i]);
+      navList[2].classList.add('nav-current');
+  }
+}
+
+navList[3].onclick = function() {
+  for (var i = 0; i<navList.length; i++) {
+      removeNavItem(navList[i]);
+      navList[3].classList.add('nav-current');
+  }
+}
 navList[4].onclick = function() {
   for (var i = 0; i<navList.length; i++) {
       removeNavItem(navList[i]);
       navList[4].classList.add('nav-current');
+  }
+}
+
+navList[5].onclick = function() {
+  for (var i = 0; i<navList.length; i++) {
+      removeNavItem(navList[i]);
+      navList[5].classList.add('nav-current');
+  }
+}
+
+navList[6].onclick = function() {
+  for (var i = 0; i<navList.length; i++) {
+      removeNavItem(navList[i]);
+      navList[6].classList.add('nav-current');
+  }
+}
+
+navList[7].onclick = function() {
+  for (var i = 0; i<navList.length; i++) {
+      removeNavItem(navList[i]);
+      navList[7].classList.add('nav-current');
+  }
+}
+
+navList[8].onclick = function() {
+  for (var i = 0; i<navList.length; i++) {
+      removeNavItem(navList[i]);
+      navList[8].classList.add('nav-current');
   }
 }
 
